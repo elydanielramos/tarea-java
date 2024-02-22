02d3d7d934d3c6044fbdcdf6de7d957551267edb
elydanielramos <elydanielramosanchez@gmail.com>
2024-02-22 15:15:34 +0100

    Creación de un servlet 3 para guardar lo realizado de la tarea 2, y arreglada la tarea 1.

--- src/main/java/ejemplos/servlet/curso/MyServlet.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet.java
@@ -13,10 +13,25 @@ import java.io.PrintWriter;
 public class MyServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+						 HttpServletResponse response) throws ServletException, IOException {
 
-		RequestDispatcher dispatcher = request.getRequestDispatcher("form.jsp");
-		dispatcher.forward(request, response);
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		// send HTML page to client
+		out.println("<html>");
+		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
+		out.println("<body>");
+		out.println("<h1>Formulario desde Servlet</h1>");
+		out.println("<form action=\"/myServlet2\">");
+		out.println("<label for=\"fname\">Primer Nombre:</label><br>");
+		out.println("<input type=\"text\" id=\"fname\" name=\"nombre\" value=\"Daniel\"><br>");
+		out.println("<label for=\"lname\">Apellido:</label><br>");
+		out.println("<input type=\"text\" id=\"lname\" lastname=\"apellido\" value=\"Ramos\"><br>");
+		out.println("<input type=\"submit\" value=\"Enviar\">");
+		out.println("</form>");
+		out.println("<p><a href=\"/myServlet2\">Vamos al otro Servlet</a></p>");
+		out.println("</body></html>");
 	}
 
 }
---
+++ src/main/java/ejemplos/servlet/curso/MyServlet3.java
@@ -0,0 +1,21 @@
+package ejemplos.servlet.curso;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/myServlet3")
+public class MyServlet3 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("form.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
